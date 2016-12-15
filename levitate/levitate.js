d005e73b72c32480e4243343e0e679f1b294b18a
MinskyNg <minsky199525@gmail.com>
2016-12-15 20:05:46 +0800

    改善levitate动画效果

--- levitate/levitate.js
+++ levitate/levitate.js
@@ -1,4 +1,7 @@
 (function(window, document){
+    var de = document.documentElement;
+    var db = document.body;
+
     function Levitate(opts) {
         this.banner = document.getElementById(opts.banner);
         this.speed = opts.speed;
@@ -16,17 +19,9 @@
             document.addEventListener('scroll', this._handleScroll);
         },
 
-        _handleScroll: function(ev) {
-            var oEvent = ev || event;
-            var scrollTop = document.documentElement.scrollTop + document.body.scrollTop;
+        _handleScroll: function() {
+            var scrollTop = de.scrollTop + db.scrollTop;
             this.banner.style.top = scrollTop + 'px';
-            // var moveHeight = (scrollTop - this.banner.offsetTop) / 10;
-            // moveHeight = moveHeight > 0 ? Math.ceil(moveHeight) : Math.floor(moveHeight);
-            // if (moveHeight !== 0) {
-            //     this.banner.style.top = this.banner.offsetTop + moveHeight + 'px';
-            //     this.afterMove(moveHeight);
-            //     setTimeout(this._handleScroll, this.speed);
-            // }
         },
 
         destroy: function() {
@@ -47,16 +42,15 @@
     BackTop.prototype = {
         init: function() {
             // 绑定窗口滚动事件
-            this._handleScroll = this._handleScroll.bind(this);
+            this._handleScroll = debounce(this._handleScroll.bind(this), 50);
             document.addEventListener('scroll', this._handleScroll);
             // 绑定点击事件
             this._handleClick = this._handleClick.bind(this);
             this.backTop.addEventListener('click', this._handleClick);
         },
 
-        _handleScroll: function(ev) {
-            var oEvent = ev || event;
-            var scrollTop = document.documentElement.scrollTop + document.body.scrollTop;
+        _handleScroll: function() {
+            var scrollTop = de.scrollTop + db.scrollTop;
             if (scrollTop > this.showHeight) {
                 this.backTop.style.opacity = '1';
             } else {
@@ -64,16 +58,27 @@
             }
         },
 
-        _handleClick: function(ev) {
-            document.documentElement.scrollTop = 0;
-            document.body.scrollTop = 0;
-            // var moveHeight = document.documentElement.scrollTop + document.body.scrollTop;
-            // if (moveHeight !== 0) {
-            //     moveHeight = moveHeight > 100 ? moveHeight - 100 : 0;
-            //     document.documentElement.scrollTop = moveHeight;
-            //     document.body.scrollTop = moveHeight;
-            //     setTimeout(this._handleClick, this.speed);
-            // }
+        _handleClick: function() {
+            var pre = +new Date();
+            var acc = 0;
+            function move() {
+                var top = 0;
+                var scrollTop = de.scrollTop + db.scrollTop;
+                var cur = +new Date();
+                var passed = cur - pre;
+                pre = cur;
+                acc += passed;
+                while (acc > 15 && scrollTop > 0) {
+                    top += 15 * 4;
+                    acc -= 15;
+                }
+                de.scrollTop -= top;
+                db.scrollTop -= top;
+                if (de.scrollTop + db.scrollTop > 0) {
+                    setTimeout(move, 15);
+                }
+            }
+            setTimeout(move, 15);
         },
 
         destroy: function() {
@@ -81,6 +86,21 @@
             document.removeEventListener('scroll', this._handleClick);
         }
     };
+
+
+    // 函数防抖
+    function debounce(fn, delay) {
+        var timer;
+        return function() {
+            var ctx = this,
+                args = arguments;
+            clearTimeout(timer);
+            timer = setTimeout(function() {
+                fn.apply(ctx, args);
+            }, delay || 500);
+        };
+    }
+
 
     window.BackTop = BackTop;
 })(window, document);
